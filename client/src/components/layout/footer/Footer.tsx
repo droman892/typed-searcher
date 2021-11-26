eb753af0fcf3c5a9fac73345323ffbdc3f766135
droman892 <droman892@gmail.com>
2021-11-26 18:02:10 -0500

    updated

---
+++ client/src/components/layout/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <p>hello foot</p>
+    )
+}
+
+export default Footer;
---
+++ client/src/components/pages/index/Index.tsx
@@ -0,0 +1,20 @@
+import './Index.css';
+import TopNav from '../../layout/topNav/TopNav';
+import Footer from '../../layout/footer/Footer';
+
+const Index = () => {
+    return (
+        <>
+            <TopNav/>
+
+                <main>
+                
+                </main>
+            
+
+            <Footer/>
+        </>
+    )
+}
+
+export default Index;
