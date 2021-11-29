7b68aaca0319ba4e50af0a6b405026b23b8e2f18
droman892 <droman892@gmail.com>
2021-11-28 21:37:32 -0500

    updated

--- client/src/components/layout/footer/Footer.tsx
+++ client/src/components/layout/footer/Footer.tsx
@@ -1,8 +1,20 @@
 import './Footer.css';
 
+const gemini: number = 55;
+const virgo: number = 45;
+
+
 const Footer = () => {
+    
+    
+    
     return (
-        <p>This is the Footer.</p>
+        <>
+        <p>This is the Footer.  I am years {gemini} old.</p>
+        <p>This is {virgo}.</p>
+        <h1>This is: {gemini + virgo}</h1>
+        </>
+
     )
 }
 
