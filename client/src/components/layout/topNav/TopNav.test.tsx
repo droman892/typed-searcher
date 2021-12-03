01926e548a3c41ec636b65eeb40a498405cf9d16
droman892 <droman892@gmail.com>
2021-12-03 17:48:30 -0500

    updated

--- client/src/components/layout/topNav/TopNav.test.tsx
+++ client/src/components/layout/topNav/TopNav.test.tsx
@@ -1,8 +1,7 @@
-// import React from 'react';
 import renderer from 'react-test-renderer';
 import TopNav from '../topNav/TopNav';
 
-it('***Renders Correctly***', () => {
+it('***TopNav Component Renders Correctly***', () => {
     const topNavigation = renderer.create(<TopNav/>).toJSON();
 
     expect(topNavigation).toMatchSnapshot();
