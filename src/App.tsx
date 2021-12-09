56574dd3f2f112bf93e932efb5c9e4702e6aa776
droman892 <droman892@gmail.com>
2021-12-08 22:27:44 -0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
-export function App() {
+export const App = () => {
     return (
         <div>
             <h1>
                 Now is it working?
             </h1>
         </div>
-    )
+    );
 }
