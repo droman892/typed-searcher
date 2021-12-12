661a133de5a5ce9746af4bdf8fd17f6dd5904d12
droman892 <droman892@gmail.com>
2021-12-12 18:16:15 -0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import './styles.css';
-import MediaImg from './media.png';
-import SunSVG from './sun.svg';
+import './styles.css'
+import MediaImg from './media.png'
+import SunSVG from './sun.svg'
+import { Counter } from './Counter'
 
 export const App = () => {
+    const person = 'Tech Wiz'
     return (
         <div>
             <h1>
-                It's working!!!! - {process.env.NODE_ENV} {process.env.name}
+                WOW, the app is working, {person}!!!! - {process.env.NODE_ENV}{' '}
+                {process.env.name}
             </h1>
-            <img src={MediaImg} alt='Media Icons PNG'></img>
-            <img src={SunSVG} alt='Sun SVG'></img>
+            <Counter />
+            <img src={MediaImg} alt="Media Icons PNG"></img>
+            <img src={SunSVG} alt="Sun SVG"></img>
         </div>
-    );
+    )
 }
