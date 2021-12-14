0ab1324056dc921715f837f5940f10be4e9ee814
droman892 <droman892@gmail.com>
2021-12-13 20:50:28 -0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import './styles.css'
-import MediaImg from './media.png'
-import SunSVG from './sun.svg'
-import { Counter } from './Counter'
+import { Counter } from './components/counter/Counter'
+import './styles/_index.scss'
+import MediaImg from './assets/media.png'
+import SunSVG from './assets/sun.svg'
 
 export const App = () => {
-    const person = 'Tech Wiz'
+    const person = 'Tech Wizzzzzz'
     return (
         <div>
             <h1>
                 WOW, the app is working, {person}!!!! - {process.env.NODE_ENV}{' '}
                 {process.env.name}
             </h1>
+            <p>Welcome to the template!!</p>
             <Counter />
             <img src={MediaImg} alt="Media Icons PNG"></img>
             <img src={SunSVG} alt="Sun SVG"></img>
--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,9 +1,11 @@
+import './Counter.scss'
 import { useState } from 'react'
 
 export const Counter = () => {
     const [count, setCount] = useState(0)
     return (
         <div>
+            <h1 className="button-notifier">Below is the counter:</h1>
             <button onClick={() => setCount((c) => c + 1)}>
                 Count: {count}
             </button>
