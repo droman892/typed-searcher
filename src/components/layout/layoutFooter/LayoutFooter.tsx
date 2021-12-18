32ee593162cd98be3e37d70d4d6843616feaea28
droman892 <droman892@gmail.com>
2021-12-18 13:15:09 -0500

    updated

--- src/components/layout/layoutFooter/LayoutFooter.tsx
+++ src/components/layout/layoutFooter/LayoutFooter.tsx
@@ -2,7 +2,70 @@ import './LayoutFooter.scss'
 import '../../../styles/_index.scss'
 import 'materialize-css'
 import { Footer } from 'react-materialize'
+import footerLeaf from '../../../assets/images/footerLeaf.png'
 
 export const LayoutFooter = () => {
-    return <Footer></Footer>
+    return (
+        <Footer
+            moreLinks={
+                <>
+                    <span className="left left-footer-container">
+                        <span className="footer-text">
+                            <a href="/#" className="footer-link">
+                                Advertising
+                            </a>
+                        </span>
+                        <span className="footer-text">
+                            <a href="/#" className="footer-link">
+                                Business
+                            </a>
+                        </span>
+                        <span className="footer-text hide-on-small-only">
+                            <a href="/#" className="footer-link">
+                                How Search Works
+                            </a>
+                        </span>
+                        <span className="footer-text hide-on-med-and-up">
+                            <a href="/#" className="footer-link">
+                                About
+                            </a>
+                        </span>
+                    </span>
+
+                    <span className="center-footer-container hide-on-med-and-down">
+                        <span className="footer-text">
+                            <a href="/#" className="footer-link">
+                                <img
+                                    src={footerLeaf}
+                                    alt="Footer Leaf"
+                                    className="footer-leaf"
+                                />
+                                Carbon neutral since 2007
+                            </a>
+                        </span>
+                    </span>
+
+                    <span className="right right-footer-container">
+                        <span className="footer-text">
+                            <a href="/#" className="footer-link">
+                                Privacy
+                            </a>
+                        </span>
+                        <span className="footer-text">
+                            <a href="/#" className="footer-link">
+                                Terms
+                            </a>
+                        </span>
+                        <span className="footer-text">
+                            <a href="/#" className="footer-link">
+                                Settings
+                            </a>
+                        </span>
+                    </span>
+                </>
+            }
+        >
+            <p>hello</p>
+        </Footer>
+    )
 }
--- src/components/layout/layoutNavigation/LayoutNavigation.tsx
+++ src/components/layout/layoutNavigation/LayoutNavigation.tsx
@@ -1,39 +1,43 @@
 import './LayoutNavigation.scss'
 import '../../../styles/_index.scss'
 import 'materialize-css'
-import { Navbar, Icon, NavItem } from 'react-materialize'
+import { Navbar, Icon, NavItem, Button } from 'react-materialize'
 
 export const LayoutNavigation = () => {
     return (
         <header>
-            <Navbar
-                alignLinks="left"
-                id="mobile-nav"
-                menuIcon={<Icon>menu</Icon>}
-                options={{
-                    draggable: true,
-                    edge: 'left',
-                    inDuration: 250,
-                    outDuration: 200,
-                    preventScrolling: true,
-                }}
-            >
-                <NavItem href="#" className="nav-item">
-                    About
-                </NavItem>
-                <NavItem href="#" className="nav-item">
-                    Store
-                </NavItem>
-                <NavItem href="#" className="nav-item">
-                    Gmail
-                </NavItem>
-                <NavItem href="#" className="nav-item">
-                    Images
-                </NavItem>
-                <NavItem href="#" className="nav-item">
-                    <i className="fa fa-check"></i>
-                </NavItem>
-            </Navbar>
+            <div className="section">
+                <Navbar
+                    id="mobile-nav"
+                    menuIcon={<Icon>menu</Icon>}
+                    options={{
+                        draggable: true,
+                        edge: 'left',
+                        inDuration: 250,
+                        outDuration: 200,
+                        preventScrolling: true,
+                    }}
+                >
+                    <NavItem href="#" className="nav-item">
+                        About
+                    </NavItem>
+                    <NavItem href="#" className="nav-item">
+                        Store
+                    </NavItem>
+                    <NavItem href="#" className="nav-item">
+                        Gmail
+                    </NavItem>
+                    <NavItem href="#" className="nav-item">
+                        Images
+                    </NavItem>
+                    <NavItem href="#" className="nav-item">
+                        <Icon className="nav-icon">apps</Icon>
+                    </NavItem>
+                    <Button node="a" className="nav-button">
+                        <span>Sign in</span>
+                    </Button>
+                </Navbar>
+            </div>
         </header>
     )
 }
