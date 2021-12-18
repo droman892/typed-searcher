03f63a306b2117bba369981ec439167b60febfde
droman892 <droman892@gmail.com>
2021-12-18 15:13:00 -0500

    updated

--- src/components/layout/layoutFooter/LayoutFooter.tsx
+++ src/components/layout/layoutFooter/LayoutFooter.tsx
@@ -9,63 +9,67 @@ export const LayoutFooter = () => {
         <Footer
             moreLinks={
                 <>
-                    <span className="left left-footer-container">
-                        <span className="footer-text">
-                            <a href="/#" className="footer-link">
-                                Advertising
-                            </a>
+                    <div className="footer-divider">
+                        <span className="left left-footer-container">
+                            <span className="footer-text">
+                                <a href="/#" className="footer-link">
+                                    Advertising
+                                </a>
+                            </span>
+                            <span className="footer-text">
+                                <a href="/#" className="footer-link">
+                                    Business
+                                </a>
+                            </span>
+                            <span className="footer-text hide-on-small-only">
+                                <a href="/#" className="footer-link">
+                                    How Search Works
+                                </a>
+                            </span>
+                            <span className="footer-text hide-on-med-and-up">
+                                <a href="/#" className="footer-link">
+                                    About
+                                </a>
+                            </span>
                         </span>
-                        <span className="footer-text">
-                            <a href="/#" className="footer-link">
-                                Business
-                            </a>
-                        </span>
-                        <span className="footer-text hide-on-small-only">
-                            <a href="/#" className="footer-link">
-                                How Search Works
-                            </a>
-                        </span>
-                        <span className="footer-text hide-on-med-and-up">
-                            <a href="/#" className="footer-link">
-                                About
-                            </a>
-                        </span>
-                    </span>
+                    </div>
 
-                    <span className="center-footer-container hide-on-med-and-down">
-                        <span className="footer-text">
-                            <a href="/#" className="footer-link">
-                                <img
-                                    src={footerLeaf}
-                                    alt="Footer Leaf"
-                                    className="footer-leaf"
-                                />
-                                Carbon neutral since 2007
-                            </a>
+                    <div className="footer-divider">
+                        <span className="center-footer-container hide-on-med-and-down">
+                            <span className="footer-text">
+                                <a href="/#" className="footer-link">
+                                    <img
+                                        src={footerLeaf}
+                                        alt="Footer Leaf"
+                                        className="footer-leaf"
+                                    />
+                                    Carbon neutral since 2007
+                                </a>
+                            </span>
                         </span>
-                    </span>
+                    </div>
 
-                    <span className="right right-footer-container">
-                        <span className="footer-text">
-                            <a href="/#" className="footer-link">
-                                Privacy
-                            </a>
+                    <div className="footer-divider">
+                        <span className="right right-footer-container">
+                            <span className="footer-text">
+                                <a href="/#" className="footer-link">
+                                    Privacy
+                                </a>
+                            </span>
+                            <span className="footer-text">
+                                <a href="/#" className="footer-link">
+                                    Terms
+                                </a>
+                            </span>
+                            <span className="footer-text">
+                                <a href="/#" className="footer-link">
+                                    Settings
+                                </a>
+                            </span>
                         </span>
-                        <span className="footer-text">
-                            <a href="/#" className="footer-link">
-                                Terms
-                            </a>
-                        </span>
-                        <span className="footer-text">
-                            <a href="/#" className="footer-link">
-                                Settings
-                            </a>
-                        </span>
-                    </span>
+                    </div>
                 </>
             }
-        >
-            <p>hello</p>
-        </Footer>
+        ></Footer>
     )
 }
