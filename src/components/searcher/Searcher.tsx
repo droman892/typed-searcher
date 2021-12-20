d8ac3f0d9a40d603abeb95ca559209fc42142c5a
droman892 <droman892@gmail.com>
2021-12-20 16:39:15 -0500

    updated

--- src/components/searcher/Searcher.tsx
+++ src/components/searcher/Searcher.tsx
@@ -2,11 +2,37 @@ import './Searcher.scss'
 import '../../../src/styles/_index.scss'
 import 'materialize-css'
 import { TextInput } from 'react-materialize'
+import magnifyingGlass from '../../assets/images/magnifyingGlass.png'
+import microphone from '../../assets/images/microphone.png'
 
 export const Searcher = () => {
     return (
         <div className="search-engine">
-            <TextInput id="query" inputClassName="search-input" />
+            <div className="search-engine-inner">
+                <div className="magnifying-glass-container">
+                    <div className="magnifying-glass-container-2">
+                        <span className="glass-icon-container">
+                            <img
+                                src={magnifyingGlass}
+                                alt="Magnifying Glass"
+                                className="glass-image"
+                            />
+                        </span>
+                    </div>
+                </div>
+                <div className="input-container">
+                    <TextInput id="query" inputClassName="search-input" />
+                </div>
+                <div className="microphone-container">
+                    <div className="microphone-container-2">
+                        <img
+                            src={microphone}
+                            alt="Microphone"
+                            className="microphone"
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
