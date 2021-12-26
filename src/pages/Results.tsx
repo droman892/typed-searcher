5b8a5487b8de5e585c043fdf0815a3ab5c1a5808
droman892 <droman892@gmail.com>
2021-12-26 15:01:52 -0500

    updated

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,6 +1,6 @@
 import { ResultsNavigation } from '../components/layout/resultsNavigation/ResultsNavigation'
 import { ResultsFooter } from '../components/layout/resultsFooter/ResultsFooter'
-import { ResultsContainer } from '../containers/resultsContainer/ResultsContainer'
+// import { ResultsContainer } from '../containers/resultsContainer/ResultsContainer'
 
 export const Results = () => {
     return (
@@ -8,7 +8,7 @@ export const Results = () => {
             <ResultsNavigation />
             <main>
                 <h1>This is the Results page.</h1>
-                <ResultsContainer />
+                {/* <ResultsContainer /> */}
             </main>
             <ResultsFooter />
         </div>
