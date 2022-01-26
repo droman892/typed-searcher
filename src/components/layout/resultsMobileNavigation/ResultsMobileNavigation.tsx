c4f4805d5433bd566e54486adb251656fc6dfb45
droman892 <droman892@gmail.com>
2022-01-26 17:07:56 -0500

    updated

--- src/components/layout/resultsMobileNavigation/ResultsMobileNavigation.tsx
+++ src/components/layout/resultsMobileNavigation/ResultsMobileNavigation.tsx
@@ -2,22 +2,30 @@ import './ResultsMobileNavigation.scss'
 import magnifyingGlass from '../../../assets/images/magnifyingGlass.png'
 import theX from '../../../assets/images/theX.png'
 import { TextInput } from 'react-materialize'
-import { useSelector } from 'react-redux'
-import { State } from '../../../state'
+import { useDispatch, useSelector } from 'react-redux'
+import { actionCreators, State } from '../../../state'
 import { createQuery } from '../../../state/actionCreators'
+import { bindActionCreators } from 'redux'
+import { Link, useNavigate } from 'react-router-dom'
+import { DoNothing } from '../../../functions/DoNothing'
 
 export const ResultsMobileNavigation = () => {
     const queryValue = useSelector((state: State) => state.searchQuery)
     const queryValueLength: number = queryValue.length || 0
+    let queryPath = queryValue
+    queryPath = queryPath.replace(/\s+/g, '+')
+    const dispatch = useDispatch()
+    const { createQuery } = bindActionCreators(actionCreators, dispatch)
 
     const queryString = window.location.search
     const urlParams = new URLSearchParams(queryString)
     const queryMade = urlParams.get('q') || ''
     // console.log('MOBILE - ' + queryMade)
+    const navigate = useNavigate()
 
     const InputMobileChange = (e: { target: { value: string } }) => {
         createQuery(e.target.value)
-        console.log('Mobile - input change')
+        // console.log('Mobile - input change')
     }
 
     const ClickMobileResultTrends = () => {
@@ -49,12 +57,35 @@ export const ResultsMobileNavigation = () => {
         console.log('DeleteMobileResultsQuery wrote this')
     }
 
+    const MobileResultsPressEnter = () => {
+        if (queryValueLength > 0) {
+            console.log('Mobile Results form submitted!')
+            navigate('/search?q=' + queryValue)
+        }
+    }
+
+    const SetMobileResultsURLPath = () => {
+        const inputValue = document.getElementById('query')
+        document.title = `${queryValue} - Google`
+        inputValue?.blur()
+        console.log('SetMobileResultsURLPath wrote this')
+    }
+
     return (
-        <form className="mobile-results-form">
+        <form
+            className="mobile-results-form"
+            onSubmit={MobileResultsPressEnter}
+        >
             <div>
                 <div className="mobile-results-form-2">
                     <div className="mobile-results-form-3">
-                        <button className="mobile-results-glass">
+
+
+
+                    <>
+                                {queryValueLength === 0 ? (
+
+                        <button className="mobile-results-glass" onClick={DoNothing}>
                             <div className="mobile-results-glass-2">
                                 <span className="mobile-results-glass-3">
                                     <img
@@ -65,14 +96,45 @@ export const ResultsMobileNavigation = () => {
                                 </span>
                             </div>
                         </button>
+
+                                ): (
+
+
+                                    <Link
+                                        to={{
+                                            pathname: '/search',
+                                            search: '?q=' + queryPath,
+                                        }}
+                                        onClick={SetMobileResultsURLPath}
+                                        className="mobile-results-glass"
+                                    >
+
+                                    <button className="mobile-results-glass">
+                                    <div className="mobile-results-glass-2">
+                                        <span className="mobile-results-glass-3">
+                                            <img
+                                                src={magnifyingGlass}
+                                                alt="Magnifying Glass"
+                                                className="mobile-results-glass-4"
+                                            ></img>
+                                        </span>
+                                    </div>
+                                </button>
+</Link>
+                                )}
+                        </>
+
+
+
+
                         <div className="mobile-results-input">
                             <div className="mobile-results-input-2">
                                 <TextInput
                                     inputClassName="mobile-results-input-3"
                                     // id="query"
-                                    value={queryMade || ''}
+                                    value={queryValue || ''}
                                     onChange={InputMobileChange}
-                                    onClick={ClickMobileResultTrends}
+                                    // onClick={ClickMobileResultTrends}
                                     maxLength="2048"
                                     autoComplete="off"
                                 />
